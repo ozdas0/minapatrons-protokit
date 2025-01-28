88424b68b7adfe68d369ada71c97a0a98c37a0ed
Süleyman Özdaş <ozdassuleyman123@gmail.com>
2025-01-28 15:29:28 +0300

    commit

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -5,6 +5,6 @@ dotenv.config();
 export const db = mysql.createPool({
   host: process.env.MYSQL_HOST || "localhost",
   user: process.env.MYSQL_USER || "root",
-  password: process.env.MYSQL_PASSWORD || "SuleymaN2002",
-  database: process.env.MYSQL_DATABASE || "contents_app",
+  password: process.env.MYSQL_PASSWORD || "",
+  database: process.env.MYSQL_DATABASE || "minapatorns_db",
 });
